fcfde737d9e122d18ac8b3732b2327911ed05ac8
dnaeuuon <codnjs842@naver.com>
2025-01-23 00:43:29 +0900

    feat: '홈' 페이지 작성

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+const menuHome = () => {
+    document.getElementById("contentFrame").setAttribute("src","home.html")
+    document.getElementById("menuHome").style = "color: black; background-color: white"    
+  }
