12a437548e0b24d73f65a542c5d94efa6735c0fa
dnaeuuon <codnjs842@naver.com>
2025-01-25 02:41:51 +0900

    chore: 크기 조정
    right page, index 크기 조정
    + 미니룸 사진 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,65 +1,65 @@
 const menuHome = () => {
     document.getElementById("contentFrame").setAttribute("src","home.html")
 
-    document.getElementById("menuHome").style = "color: black; background-color: white; z-index: 2;"
-    document.getElementById("menuProfile").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;"  
-    document.getElementById("menuDiary").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;"  
-    document.getElementById("menuJukeBox").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;"  
-    document.getElementById("menuPhoto").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;" 
-    document.getElementById("menuGuestBook").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;"  
+    document.getElementById("menuHome").style = "color: black; z-index: 2;"
+    document.getElementById("menuProfile").style = "color: black; z-index: 1;"  
+    document.getElementById("menuDiary").style = "color: black; z-index: 1;"  
+    document.getElementById("menuJukeBox").style = "color: black; z-index: 1;"  
+    document.getElementById("menuPhoto").style = "color: black; z-index: 1;" 
+    document.getElementById("menuGuestBook").style = "color: black; z-index: 1;"  
 }
 
 const menuProfile = () => {
   document.getElementById("contentFrame").setAttribute("src","home.html")
 
-  document.getElementById("menuHome").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;"
-  document.getElementById("menuProfile").style = "color: black; background-color: white; z-index: 2;"  
-  document.getElementById("menuDiary").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;"  
-  document.getElementById("menuJukeBox").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;"  
-  document.getElementById("menuPhoto").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;" 
-  document.getElementById("menuGuestBook").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;"      
+  document.getElementById("menuHome").style = "color: black; z-index: 1;"
+  document.getElementById("menuProfile").style = "color: black; z-index: 2;"  
+  document.getElementById("menuDiary").style = "color: black; z-index: 1;"  
+  document.getElementById("menuJukeBox").style = "color: black; z-index: 1;"  
+  document.getElementById("menuPhoto").style = "color: black; z-index: 1;" 
+  document.getElementById("menuGuestBook").style = "color: black; z-index: 1;"      
 }
 
 const menuDiary = () => {
   document.getElementById("contentFrame").setAttribute("src","home.html")
 
   document.getElementById("menuHome").style = "color: black; background-color: rgb(255, 255, 255, 0.7);; z-index: 1;"
-  document.getElementById("menuProfile").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;"  
-  document.getElementById("menuDiary").style = "color: black; background-color: white; z-index: 2;"  
-  document.getElementById("menuJukeBox").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;"  
-  document.getElementById("menuPhoto").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;" 
-  document.getElementById("menuGuestBook").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;"     
+  document.getElementById("menuProfile").style = "color: black; z-index: 1;"  
+  document.getElementById("menuDiary").style = "color: black; z-index: 2;"  
+  document.getElementById("menuJukeBox").style = "color: black; z-index: 1;"  
+  document.getElementById("menuPhoto").style = "color: black; z-index: 1;" 
+  document.getElementById("menuGuestBook").style = "color: black; z-index: 1;"     
 }
 
 const menuJukeBox = () => {
   document.getElementById("contentFrame").setAttribute("src","home.html")
 
   document.getElementById("menuHome").style = "color: black; background-color: rgb(255, 255, 255, 0.7);; z-index: 1;"
-  document.getElementById("menuProfile").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;"  
-  document.getElementById("menuDiary").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;"  
-  document.getElementById("menuJukeBox").style = "color: black; background-color: white; z-index: 2;"  
-  document.getElementById("menuPhoto").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;" 
-  document.getElementById("menuGuestBook").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;"     
+  document.getElementById("menuProfile").style = "color: black; z-index: 1;"  
+  document.getElementById("menuDiary").style = "color: black; z-index: 1;"  
+  document.getElementById("menuJukeBox").style = "color: black; z-index: 2;"  
+  document.getElementById("menuPhoto").style = "color: black; z-index: 1;" 
+  document.getElementById("menuGuestBook").style = "color: black; z-index: 1;"     
 }
 
 const menuPhoto = () => {
   document.getElementById("contentFrame").setAttribute("src","home.html")
 
   document.getElementById("menuHome").style = "color: black; background-color: rgb(255, 255, 255, 0.7);; z-index: 1;"
-  document.getElementById("menuProfile").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;"  
-  document.getElementById("menuDiary").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;"  
-  document.getElementById("menuJukeBox").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;"  
-  document.getElementById("menuPhoto").style = "color: black; background-color: white; z-index: 2;" 
-  document.getElementById("menuGuestBook").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;"     
+  document.getElementById("menuProfile").style = "color: black; z-index: 1;"  
+  document.getElementById("menuDiary").style = "color: black; z-index: 1;"  
+  document.getElementById("menuJukeBox").style = "color: black; z-index: 1;"  
+  document.getElementById("menuPhoto").style = "color: black; z-index: 2;" 
+  document.getElementById("menuGuestBook").style = "color: black; z-index: 1;"     
 }
 
 const menuGuestBook = () => {
   document.getElementById("contentFrame").setAttribute("src","home.html")
   
   document.getElementById("menuHome").style = "color: black; background-color: rgb(255, 255, 255, 0.7);; z-index: 1;"
-  document.getElementById("menuProfile").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;"  
-  document.getElementById("menuDiary").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;"  
-  document.getElementById("menuJukeBox").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;"  
-  document.getElementById("menuPhoto").style = "color: black; background-color: rgb(255, 255, 255, 0.7); z-index: 1;" 
-  document.getElementById("menuGuestBook").style = "color: black; background-color: white; z-index: 2;"     
+  document.getElementById("menuProfile").style = "color: black; z-index: 1;"  
+  document.getElementById("menuDiary").style = "color: black; z-index: 1;"  
+  document.getElementById("menuJukeBox").style = "color: black; z-index: 1;"  
+  document.getElementById("menuPhoto").style = "color: black; z-index: 1;" 
+  document.getElementById("menuGuestBook").style = "color: black; z-index: 2;"     
 }
