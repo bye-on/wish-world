2492ac3b85be993fc4dc493243671345a42ebd46
bye-on <codnjs842@naver.com>
2025-02-18 16:22:52 +0900

    feat: firebase 초기 설정

--- src/js/jukebox.js
+++ src/js/jukebox.js
@@ -1,5 +1,113 @@
 const API_KEY = 'YOUR_YOUTUBE_API_KEY';
-let player;
+
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/embed/";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+        
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+    height: '0',
+    width: '0',
+    videoId: 'u0JxxLZpc3E',
+    playerVars: {
+        autoplay : 1,
+        loop: 1,
+        playlist: 'u0JxxLZpc3E',
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+        });
+    }
+
+function addSong()
+{
+    const url = document.getElementById('path').value;
+    const title = document.getElementById('title').value;
+    const artist = document.getElementById('artist').value;
+
+    if (!title || !artist || !url) return alert("올바른 정보를 입력하세요!");
+            
+    const songList = document.getElementById('songList');
+    const li = document.createElement('li');
+    li.innerHTML = `<input type="checkbox" class="song-check"> ${title} - ${artist}`;
+    songList.appendChild(li);
+
+    fetch("http://localhost:3000/jukebox", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({url, title, artist})
+    })
+    .then(res => res.json())
+    .then(data => {
+        if(data.success)
+        {
+            document.getElementById("songList").reset();
+            loadSongList();
+        }
+    })
+    .catch(error => console.error("Error:", error));
+}
+
+function loadSongList()
+{
+    fetch("http://localhost:3000/jukebox")
+    .then(res => res.json())
+    .then(data => {
+        const guestBookList = document.getElementById("songList");
+        guestBookList.innerHTML = ""; // 기존 목록 초기화
+
+        data.forEach(post => {
+            const postElement = document.createElement("div");
+            postElement.className = "song-post";
+            postElement.innerHTML = `
+            
+            `;
+            guestBookList.appendChild(postElement);
+        });
+    })
+    .catch(error => console.error("Error:", error));
+}
+
+
+        function onPlayerStateChange(event) {
+            if (event.data == YT.PlayerState.ENDED) {
+            console.log("onPlayerStateChange:",event.data,"ENDED");
+            } else if (event.data == YT.PlayerState.PLAYING) {
+            console.log("onPlayerStateChange:",event.data,"PLAYING");
+            } else if (event.data == YT.PlayerState.PAUSED) {
+            console.log("onPlayerStateChange:",event.data,"PAUSED");
+            } else if (event.data == YT.PlayerState.BUFFERING) {
+            console.log("onPlayerStateChange:",event.data,"BUFFERING");
+            } else if (event.data == YT.PlayerState.CUED) {
+            console.log("onPlayerStateChange:",event.data,"CUED");
+            }
+        }
+
+
+        // 4. The API will call this function when the video player is ready.
+        function onPlayerReady(event) {
+            event.target.playVideo();
+        }
+
+        // 5. The API calls this function when the player's state changes.
+        //    The function indicates that when playing a video (state=1),
+        //    the player should play for six seconds and then stop.
+        var done = false;
+        function onPlayerStateChange(event) {
+            if (event.data == YT.PlayerState.PLAYING && !done) {
+            // setTimeout(stopVideo, 6000);
+            // done = true;
+            }
+        }
+        function stopVideo() {
+            player.stopVideo();
+        }
 
 function searchMusic() {
     const query = document.getElementById('search').value;
