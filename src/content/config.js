2266c7ea817c384aaa154fa9da53f412044c9b2a
bye-on <codnjs842@naver.com>
2025-02-26 23:35:19 +0900

    chore: add config.js

---
+++ src/content/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAusHTZq3khKqvsy8lqQzNlxyZqw3A2w7E",
+    authDomain: "wish-world-desu.firebaseapp.com",
+    databaseURL: "https://wish-world-desu-default-rtdb.firebaseio.com",
+    projectId: "wish-world-desu",
+    storageBucket: "wish-world-desu.firebasestorage.app",
+    messagingSenderId: "640167301597",
+    appId: "1:640167301597:web:56c5b74e74c3da4bf7a634",
+    measurementId: "G-D6HXKBP9NP"
+};
+
+export default firebaseConfig;
